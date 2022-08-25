dd8cbe76de5155974fb509b20e21e0900587a05a
JustValk <50841876+JustValk@users.noreply.github.com>
2022-08-25 13:18:56 +0100

    Update Aiming.lua

--- Aiming.lua
+++ Aiming.lua
@@ -445,11 +445,11 @@ RunService.RenderStepped:Connect(function ()
                 end)
             end
                 if DaHoodSettings.ReverseResolver == true then
-                    local zxc = CFrame.new(workspace.CurrentCamera.CFrame.p, ClosestPartPosition - ClosestPartPosition.Velocity/DaHoodSettings.Prediction)
+                    local zxc = CFrame.new(workspace.CurrentCamera.CFrame.p, ClosestPartPosition - Aiming.Character.Velocity/DaHoodSettings.Prediction)
                     workspace.CurrentCamera.CFrame = workspace.CurrentCamera.CFrame:Lerp(zxc, 1, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut)
                 end
                 if DaHoodSettings.ReverseResolver == false then
-                    local zxc = CFrame.new(workspace.CurrentCamera.CFrame.p, ClosestPartPosition + ClosestPartPosition.Velocity/DaHoodSettings.Prediction)
+                    local zxc = CFrame.new(workspace.CurrentCamera.CFrame.p, ClosestPartPosition + Aiming.Character.Velocity/DaHoodSettings.Prediction)
                     workspace.CurrentCamera.CFrame = workspace.CurrentCamera.CFrame:Lerp(zxc, 1, Enum.EasingStyle.Elastic, Enum.EasingDirection.InOut)
                 end
     end)
