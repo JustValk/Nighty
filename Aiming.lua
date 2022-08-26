d25e496eeb214cb0f702e5d58545bed0a30e0b4d
JustValk <50841876+JustValk@users.noreply.github.com>
2022-08-26 16:30:47 +0100

    Update Aiming.lua

--- Aiming.lua
+++ Aiming.lua
@@ -401,7 +401,7 @@ function Aiming.GetClosestPlayerToCursor()
     -- // Loop through all players
     for _, Player in ipairs(GetPlayers(Players)) do
         -- // Get Character
-        local Character = Aiming.Selected(Player) or Aiming.CharacterAdded:Wait()
+        local Character = Aiming.ClosestPlayer(Player) or ClosestPlayer.CharacterAdded:Wait()
 
 -- // Resolver part
 TargetVelocity = Aiming.Character.HumanoidRootPart.Velocity
