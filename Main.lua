e266d5e02c5c55d41b640854bed20b3887f93845
JustValk <50841876+JustValk@users.noreply.github.com>
2022-09-04 14:53:46 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -17,7 +17,7 @@ local Workspace = game:GetService("Workspace")
 local CurrentCamera = Workspace.CurrentCamera
 
 local DaHoodSettings = {
-    Prediction = 0.135,
+    Prediction = 0.125,
 
     SilentAim = true,
 
