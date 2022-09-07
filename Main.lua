4face4947fa9184c887e52ae5a024efaaeec9f89
JustValk <50841876+JustValk@users.noreply.github.com>
2022-09-07 18:45:30 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,7 +5,7 @@ local AimingChecks = Aiming.Checks
 local AimingSelected = Aiming.Selected
 
  -- // Makes sure that it aint being seen thanks to stefanuk huge thanks to him!
-Aiming.Settings.FOVSettings.Scale = 18
+Aiming.Settings.FOVSettings.Scale = 10
 Aiming.Settings.FOVSettings.Sides = 25
 Aiming.Settings.FOVSettings.Enabled = false
 Aiming.Settings.TracerSettings.Enabled = false
