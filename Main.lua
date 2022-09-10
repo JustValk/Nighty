ad35288d480d059d66e4186d9883b1bcf8817594
JustValk <50841876+JustValk@users.noreply.github.com>
2022-09-10 09:27:16 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,7 +5,7 @@ local AimingChecks = Aiming.Checks
 local AimingSelected = Aiming.Selected
 
  -- // Makes sure that it aint being seen thanks to stefanuk huge thanks to him!
-Aiming.Settings.FOVSettings.Scale = 12.4
+Aiming.Settings.FOVSettings.Scale = 12.44
 Aiming.Settings.FOVSettings.Sides = 25
 Aiming.Settings.FOVSettings.Enabled = false
 Aiming.Settings.TracerSettings.Enabled = false
@@ -17,7 +17,7 @@ local Workspace = game:GetService("Workspace")
 local CurrentCamera = Workspace.CurrentCamera
 
 local DaHoodSettings = {
-    Prediction = 0.12,
+    Prediction = 0.148357,
 
     SilentAim = true,
 
